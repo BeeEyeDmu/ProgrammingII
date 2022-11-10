ce322d549cb5dacb6f99be9fae653c6a1d78231a
BeeEye Dmu <bikang@konyang.ac.kr>
2022-11-10 10:51:25 +0900

    Add files via upload

---
+++ w11_129_overloading/w11_129_overloading.cpp
@@ -0,0 +1,38 @@
+﻿// w11_129_overloading.cpp : 이 파일에는 'main' 함수가 포함됩니다. 거기서 프로그램 실행이 시작되고 종료됩니다.
+//
+
+#include <iostream>
+using namespace std;
+
+void swap(int &a, int &b)
+{
+	int tmp = a;
+	a = b;
+	b = tmp;
+}
+void swap(double &a, double &b)
+{
+	double tmp = a;
+	a = b;
+	b = tmp;
+}
+void swap(char &a, char &b)
+{
+	char tmp = a;
+	a = b;
+	b = tmp;
+}
+int main()
+{
+	int a = 10, b = 20;
+	double da = 12.34, db = 56.78;
+	char ca = 'a', cb = 'b';
+
+	swap(a, b);
+	swap(da, db);
+	swap(ca, cb);
+
+	cout << a << " " << b << endl;
+	cout << da << " " << db << endl;
+	cout << ca << " " << cb << endl;
+}
---
+++ w11_131_CPPsruct/w11_131_CPPsruct.cpp
@@ -0,0 +1,53 @@
+﻿// w11_131_CPPsruct.cpp : 이 파일에는 'main' 함수가 포함됩니다. 거기서 프로그램 실행이 시작되고 종료됩니다.
+//
+
+#include <iostream>
+using namespace std;
+
+class TV {
+private:
+	bool powerOn;
+	int channel;
+	int volume;
+public:
+	void on() {
+		powerOn = true;
+	}
+	void off() {
+		powerOn = false;
+	}
+	void setChannel(int ch) {
+		if(ch >= 1 && ch <= 999)
+			channel = ch;
+	}
+	int getChannel() {
+		return channel;
+	}
+	void setVolume(int vol) {
+		if(vol >= 0 && vol <= 100)
+			volume = vol;
+	}
+	int getVolume() {
+		return volume;
+	}
+};
+int main()
+{
+	TV myTv;  // struct 생략가능
+
+/*	myTv.powerOn = true;
+	myTv.channel = -10;
+	myTv.volume = 5000;
+
+	cout << "현재 채널은 " << myTv.channel << endl;
+	cout << "현재 볼륨은 " << myTv.volume << endl;*/
+
+	myTv.on();
+	myTv.setChannel(10);
+	myTv.setVolume(50);
+
+	cout << "현재 채널은 " 
+		<< myTv.getChannel() << endl;
+	cout << "현재 볼륨은 "
+		<< myTv.getVolume() << endl;
+}
