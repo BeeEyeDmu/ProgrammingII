c6fb6176e525c7d5a43b80ccefcc0b6fb22cd2e4
BeeEye Dmu <bikang@konyang.ac.kr>
2022-12-01 10:54:23 +0900

    Add files via upload

---
+++ w14_156_fuctionPointer/w14_156_fuctionPointer.cpp
@@ -0,0 +1,55 @@
+﻿// w14_156_fuctionPointer.cpp : 이 파일에는 'main' 함수가 포함됩니다. 거기서 프로그램 실행이 시작되고 종료됩니다.
+//
+
+#include <iostream>
+using namespace std;
+
+int square(int n) {
+	return n * n;
+}
+
+int myFunc(int n) {
+	return n * (n - 15) / 2;
+}
+
+// 배열에서 최소값을 찾아서 리턴하는 함수
+int arrMin(int a[], int n) {
+	int min = a[0];
+	for (int i = 1; i < 7; i++)
+		if (a[i] < min)
+			min = a[i];
+	return min;
+}
+int arrSqrMin(int a[], int n) {
+	int min = a[0]*a[0];
+	for (int i = 1; i < 7; i++)
+		if (a[i]*a[i] < min)
+			min = a[i]*a[i];
+	return min;
+}
+
+int arrFnMin(int a[], int n, int (*f)(int)) {
+	int min = f(a[0]);
+	for (int i = 1; i < n; i++)
+		if (min > f(a[i]))
+			min = f(a[i]);
+	return min;
+}
+
+int main()
+{
+	int a[7] = { 3, 5, 7, -2, -9, 4, 8 };
+	int min;
+
+	min = arrMin(a, 7);	// a[]에서 최소값을 찾는 함수
+	cout << min << endl;
+
+	min = arrSqrMin(a, 7);	// a[]에서 최소값을 찾는 함수
+	cout << min << endl;
+
+	min = arrFnMin(a, 7, square);	// a[]에서 최소값을 찾는 함수
+	cout << min << endl;
+
+	min = arrFnMin(a, 7, myFunc);	// a[]에서 최소값을 찾는 함수
+	cout << min << endl;
+}
---
+++ w14_157_vector/w14_157_vector.cpp
@@ -0,0 +1,37 @@
+﻿// w14_157_vector.cpp : 이 파일에는 'main' 함수가 포함됩니다. 거기서 프로그램 실행이 시작되고 종료됩니다.
+//
+
+#include <iostream>
+#include <vector>
+#include <utility>
+using namespace std;
+
+int main()
+{
+	vector<int> v;
+	v.push_back(1);
+	v.push_back(2);
+	v.push_back(3);
+
+	vector<double> w = { 1.1, 2.2, 3.3 };
+
+	for (int i : v)
+		cout << i << " ";
+	cout << endl;
+
+	for (int i = 0; i < 3; i++)
+		cout << v[i] << " ";
+	cout << endl;
+
+	for (double d : w)
+		cout << d << " ";
+	cout << endl;
+
+	pair<int, string> a = { 123, "abc" };
+	cout << a.first << " ";
+	cout << a.second << endl;
+
+	a.second = "def";
+	cout << a.first << " " << a.second << endl;
+
+}
